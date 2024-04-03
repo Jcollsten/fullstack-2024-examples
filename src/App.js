70348f0ec8aa9471e841ed17539a63dbbaec2a8d
Jonas <jonas.collsten@gmail.com>
2024-04-03 10:00:40 +0200

    In this commmit i created the first commit for this project

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img
+          src={logo}
+          className='App-logo'
+          alt='logo'
+        />
+        <p>I hope to learn how to create and navigate a React-App</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
